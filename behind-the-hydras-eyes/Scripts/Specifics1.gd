4264fa395cd3278f4383c328842edccf0def1614
Dragos-F <160853098+Dragos-F@users.noreply.github.com>
2025-06-27 23:04:53 +0100

    Latest Text Changes

--- behind-the-hydras-eyes/Scripts/Specifics1.gd
+++ behind-the-hydras-eyes/Scripts/Specifics1.gd
@@ -1,4 +1,4 @@
-extends Node
+extends Node2D
 @export var alexInteracted:bool = false
 @export var AlexNotif:AnimatedSprite2D
 @export var jenInteracted:bool = false
@@ -6,10 +6,14 @@ extends Node
 @export var emails:Computer
 @export var outsideDoor:Interactable
 @export var DoorNotif:AnimatedSprite2D
+@export var brain:DayBrain
+@export var BossTextAnchor:Node2D
 
 
 func _ready() -> void:
-	pass
+	var layout = Dialogic.start("Text From Boss")
+	layout.register_character("res://Dialogue stuffs/Dialogic/Characters/Office/Text From Boss.dch",BossTextAnchor)
+	
 	
 func _process(delta: float) -> void:
 	if (alexInteracted and jenInteracted and emails.emailsDone==emails.emailsProgressionQuota):
