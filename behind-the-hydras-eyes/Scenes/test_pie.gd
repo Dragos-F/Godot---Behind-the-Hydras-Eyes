cbaf2ffcac078894588dd5d31715050fe7748a2d
Dragos-F <160853098+Dragos-F@users.noreply.github.com>
2025-07-02 22:49:35 +0100

    Budget Minigame complete
    Final draft of the budget minigame.
    Font and polish required but mechanically complete.

--- behind-the-hydras-eyes/Scenes/test_pie.gd
+++ behind-the-hydras-eyes/Scenes/test_pie.gd
@@ -3,8 +3,15 @@ extends Control
 @export var chart:LabellessPieChart
 @export var colors:Array[Color]
 @export var values:Array[float]
-@export var buttonDifference:float = 0.05 # var used to increase/decrease buttons
+@export var buttonDifference:float = 0.02 # var used to increase/decrease buttons
+@export var savingsText:Label 
+@export var totalSalary:float
+@export var startingSavings:float
 
+
+func _ready() -> void:
+	savingsText.text = str(startingSavings)
+	
 func _process(delta: float) -> void:
 	chart.all_entries = [
 			LabellessPieChartEntry.new(values[0],colors[0]),
@@ -12,21 +19,36 @@ func _process(delta: float) -> void:
 			LabellessPieChartEntry.new(values[2],colors[2]),
 			LabellessPieChartEntry.new(values[3],colors[3])
 	]
+	savingsText.text = "£"+str(startingSavings+totalSalary*values[2])
 	
 
 
 
 func _on_savings_pressed() -> void: #DECREASE
-	values[2] -=buttonDifference
-
-
+	if values[2]> 0:
+		values[2] -=buttonDifference
+		values[3] +=buttonDifference
 func _on_lifestyle_pressed() -> void: #DECREASE
-	values[3] -=buttonDifference
-
-
-
+	if values[3] > 0.05:
+		values[3] -=buttonDifference
+		values[2] +=buttonDifference
 func _on_savings_increase() -> void: #INCREASE
-	values[2] +=buttonDifference
-	
+	if values[3] > 0.05:
+		values[2] +=buttonDifference
+		values[3] -= buttonDifference
 func _on_lifestyle_increase() -> void: #INCREASE
-	values[3] +=buttonDifference
+	if values[2] > 0.05:
+		values[3] +=buttonDifference
+		values[2] -=buttonDifference
+
+
+func _on_check_button_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		values[1] += buttonDifference
+		values[2] -= buttonDifference/2
+		values[3] -= buttonDifference/2
+	else:
+		values[1] -= buttonDifference
+		values[2] += buttonDifference/2
+		values[3] += buttonDifference/2
+		
