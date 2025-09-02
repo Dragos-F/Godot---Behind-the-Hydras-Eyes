7345c5d378b62b18e9682706d1b4960cbe0add03
Dragos-F <160853098+Dragos-F@users.noreply.github.com>
2025-09-02 17:17:27 +0100

    Tea Minigame Final? No Sound

--- behind-the-hydras-eyes/Scripts/tea_minigame.gd
+++ behind-the-hydras-eyes/Scripts/tea_minigame.gd
@@ -1,9 +1,14 @@
 extends Node2D
 class_name minigame_brain
-
+@export var PauseMenu:pauseMenu
 @export var holding:bool = false
 
-
+func _input(event):
+	if event.is_action_pressed("menu"):
+		if !PauseMenu.visible:
+			PauseMenu.openPause()
+		elif PauseMenu.visible:
+			PauseMenu.closePause()
 
 func _on_button_pressed() -> void:
 	Fader.FadeUp("Office")
